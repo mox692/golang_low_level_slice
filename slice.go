3be83b4834105512e51c37c34e10b38bf6838827
mox692 <55653825+mox692@users.noreply.github.com>
2021-05-05 16:43:48 +0900

    feat: at, get, setの実装

--- slice.go
+++ slice.go
@@ -5,13 +5,19 @@ import (
 	"unsafe"
 )
 
+const (
+	TEN   = 10
+	HND   = 100
+	THOUS = 1000
+)
+
 type slice struct {
-	Data uintptr
+	Data unsafe.Pointer
 	Len  int
 	Cap  int
 }
 
-func createslice(length, cap int, elm ...int) (*slice, error) {
+func Createslice(length, cap int, elm ...int) (*slice, error) {
 	if length < 0 || cap < 0 {
 		return nil, errors.New("len or cap must be positive number.")
 	}
@@ -20,16 +26,88 @@ func createslice(length, cap int, elm ...int) (*slice, error) {
 		return nil, errors.New("len must be less than cap.")
 	}
 
-	backgroundArr := make([]int, length)
-	if elm != nil {
-		for elmLen, i := 0, len(elm); i < elmLen; i++ {
-			backgroundArr[i] = elm[i]
+	if cap < 10 {
+		var backgroundArr [TEN]int
+		if elm != nil {
+			for elmLen, i := len(elm), 0; i < elmLen; i++ {
+				backgroundArr[i] = elm[i]
+			}
 		}
+		slice := &slice{}
+		slice.Cap = cap
+		slice.Len = length
+		slice.Data = unsafe.Pointer(&backgroundArr)
+		return slice, nil
+	} else if cap < 100 {
+		var backgroundArr [HND]int
+
+		if elm != nil {
+			for elmLen, i := len(elm), 0; i < elmLen; i++ {
+				backgroundArr[i] = elm[i]
+			}
+		}
+
+		slice := &slice{}
+		slice.Cap = cap
+		slice.Len = length
+		slice.Data = unsafe.Pointer(&backgroundArr)
+		return slice, nil
+	} else if cap < 1000 {
+		var backgroundArr [THOUS]int
+
+		if elm != nil {
+			for elmLen, i := len(elm), 0; i < elmLen; i++ {
+				backgroundArr[i] = elm[i]
+			}
+		}
+
+		slice := &slice{}
+		slice.Cap = cap
+		slice.Len = length
+		slice.Data = unsafe.Pointer(&backgroundArr)
+		return slice, nil
+	} else {
+		return nil, errors.New("cannot create slice which cap is over 1000.")
+	}
+}
+
+func (s *slice) at(index int) (unsafe.Pointer, error) {
+	if index < 0 {
+		return nil, errors.New("invalid index value, at's index must be positive number.")
+	}
+	if index > s.Cap-1 {
+		return nil, errors.New("invalid reference. index must be less than cap.")
 	}
 
-	slice := &slice{}
-	slice.Cap = cap
-	slice.Len = length
-	slice.Data = uintptr(unsafe.Pointer(&backgroundArr))
-	return slice, nil
+	return unsafe.Pointer(uintptr(s.Data) + uintptr(index)*unsafe.Sizeof(int(0))), nil
+}
+
+func (s *slice) Get(index int) (int, error) {
+	if index < 0 {
+		return 0, errors.New("index must be positive value.")
+	}
+	if index > s.Len {
+		return 0, errors.New("index must be less than slice's cap.")
+	}
+	ptr, err := s.at(index)
+	if err != nil {
+		return 0, err
+	}
+	return *(*int)(ptr), nil
+}
+
+func (s *slice) Set(index, value int) error {
+	if index < 0 {
+		return errors.New("index must be positive value.")
+	}
+	if index > s.Len {
+		return errors.New("index must be less than slice's cap.")
+	}
+	ptr, err := s.at(index)
+	if err != nil {
+		return err
+	}
+	*(*int)(ptr) = value
+
+	return nil
 }
--- slice_test.go
+++ slice_test.go
@@ -1,9 +1,12 @@
 package slice
 
 import (
-	"fmt"
 	"testing"
 )
+
+/*
+sliceのpointerまでをtestするのは難しいので、errが返ってくるか返ってこないかでtestを行う
+*/
 
 type testInput struct {
 	len int
@@ -22,7 +25,6 @@ func Test_Createslice(t *testing.T) {
 	}
 
 	for i, v := range inputs {
-		fmt.Println(len(inputs))
 		ptr, err := Createslice(v.len, v.cap, v.elm...)
 		if err == nil && ptr == nil {
 			t.Errorf("ptr is nil\n")
@@ -32,6 +34,93 @@ func Test_Createslice(t *testing.T) {
 		}
 		if err == nil && i < 0 {
 			t.Errorf("expect error(%d) but got noerr: %+v\n", i, err)
+		}
+	}
+}
+
+func Test_at(t *testing.T) {
+	slice, err := Createslice(3, 3, 1, 2, 3)
+	if err != nil {
+		t.Errorf("Createslice Err: %+v\n", err)
+	}
+	inputs := map[int]int{
+		1:  2,
+		2:  2,
+		-1: -1,
+		-2: 3,
+	}
+
+	for i, v := range inputs {
+		ptr, err := slice.at(v)
+		if i < 0 && err == nil {
+			t.Errorf("expect err(%d), but got noerr\n", i)
+		}
+		if i > 0 && ptr == nil {
+			t.Errorf("expect noerr(%d), but got noptr\n", i)
+		}
+	}
+}
+
+type getResult struct {
+	input  int
+	expect int
+}
+
+func Test_Get(t *testing.T) {
+	testSlice, err := Createslice(2, 4, 1, 2)
+	if err != nil {
+		t.Errorf("createSliceErr: %+v\n", err)
+	}
+	inputs := map[int]getResult{
+		1:  {input: 1, expect: 2},
+		2:  {input: 0, expect: 1},
+		-1: {input: 3, expect: 0},
+		-2: {input: -1, expect: 0},
+	}
+
+	for i, v := range inputs {
+		result, err := testSlice.Get(v.input)
+		if i < 0 && err == nil {
+			t.Errorf("expect err(%d), but got noerr\n", i)
+		}
+		if i > 0 && err != nil {
+			t.Errorf("expect noerr(%d), but got err: %+v\n", i, err)
+		}
+		if i > 0 && result != v.expect {
+			t.Errorf("expect %d, but got %d\n", v.expect, result)
+		}
+	}
+}
+
+type setInput struct {
+	index int
+	value int
+}
+
+func Test_Set(t *testing.T) {
+	testSlice, err := Createslice(2, 4, 1, 2)
+	if err != nil {
+		t.Errorf("createSliceErr: %+v\n", err)
+	}
+
+	inputs := map[int]setInput{
+		1:  {index: 0, value: 100},
+		2:  {index: 1, value: -10000},
+		-1: {index: -1, value: 100},
+		-2: {index: 4, value: 100},
+	}
+
+	for i, v := range inputs {
+		err := testSlice.Set(v.index, v.value)
+		result, _ := testSlice.Get(v.index)
+		if i < 0 && err == nil {
+			t.Errorf("expect err(%d), but got noerr\n", i)
+		}
+		if i > 0 && err != nil {
+			t.Errorf("expect noerr(%d), but got err: %+v\n", i, err)
+		}
+		if i > 0 && v.value != result {
+			t.Errorf("expect %d, but got %d\n", v.value, result)
 		}
 	}
 }
